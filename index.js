be95dcb10f13611321398a1b363a44544bae8465
Arman <arman.shaikh.work@gmail.com>
2024-03-15 22:50:05 +0000

    Changed port number to 8080

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const socket = require("socket.io");
 
 // App setup
-const PORT = 5000;
+const PORT = 8080;
 const app = express();
 const server = app.listen(PORT, function () {
     console.log(`Listening on port ${PORT}`);
