2d4415e936fcc20176e1c4d032d40d82e00c165e
Arman <arman.shaikh.work@gmail.com>
2024-03-23 19:10:43 +0000

    Finished of readme

--- public/js/news.js
+++ public/js/news.js
@@ -4,6 +4,7 @@ const get_news = async () => {
     const response = await fetch("/api/news");
     const data = await response.json();
     news_data = data;
+    var content = [];
     news_data.forEach((news) => {
         const news_div = document.createElement("div");
         news_div.className = "news";
@@ -11,12 +12,12 @@ const get_news = async () => {
             <h2><a href="${news.link}" target="_blank">${news.title}</a></h2>
             <h3>By: ${news.source}</h3>
             <a href="${news.link}" target="_blank"><img src="${news.og}" style="max-width: 100%;"></img></a>
-            <br/>
+            <br>
             <a href="${news.link}" target="_blank">Read more</a>
-            <hr/>
         `;
-        news_container.appendChild(news_div);
+        content.push(news_div.outerHTML);
     });
+    news_container.innerHTML = content.join("<hr>");
 };
 
 get_news();
